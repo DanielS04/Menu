28612e2f0c5303f68dd81de09110fafad6cc303c
aloix123 <69531892+aloix123@users.noreply.github.com>
2025-01-05 21:55:01 +0100

    added plots of cured and infected people

---
+++ plotCuredHeatMap.m
@@ -0,0 +1,32 @@
+function  plotCuredHeatMap(positions,cured,city_size)
+    grid_size = 10;  
+    grid_x = linspace(0, city_size(1), grid_size + 1);  
+    grid_y = linspace(0, city_size(2), grid_size + 1);  
+    infected_grid = zeros(grid_size, grid_size); 
+    
+    for i = 1:length(cured)
+        if cured(i)
+            
+            x_idx = find(positions(i, 1) <= grid_x, 1) - 1;
+            y_idx = find(positions(i, 2) <= grid_y, 1) - 1;
+            
+            if x_idx > 0 && y_idx > 0 && x_idx <= grid_size && y_idx <= grid_size
+                infected_grid(x_idx, y_idx) = infected_grid(x_idx, y_idx) + 1;
+            end
+        end
+    end
+    figure;
+    [X, Y] = meshgrid(grid_x(1:end-1), grid_y(1:end-1));
+    Z = infected_grid';  
+    Z_interp = interp2(X, Y, Z, X, Y, 'linear');  
+    surf(X, Y, Z_interp, 'EdgeColor', 'none');  
+    colorbar;
+    title('Wykres 3D uleczonych');
+    xlabel('X');
+    ylabel('Y');
+    zlabel('Liczba uleczonych');
+    view(3); 
+    shading interp;  
+
+end
+
